7bf0f54b28141998ad9502bb0fc6034a486cd122
lfqtm <740942486@qq.com>
2021-02-25 22:03:59 +0800

    dev

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 
+import Home from './pages/Home'
+
 export default class App extends Component {
     render() {
         return (
             <div>
-                app...
+                <Home />
             </div>
         )
     }
